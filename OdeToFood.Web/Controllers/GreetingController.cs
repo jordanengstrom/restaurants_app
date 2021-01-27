ee177d72e2e8fd135daf723f10554a3036e695cb
Jordan Engstrom <jordan.engstrom@intimetec.com>
2021-01-27 13:25:17 -0700

    added hello world api controller

--- OdeToFood.Web/Controllers/GreetingController.cs
+++ OdeToFood.Web/Controllers/GreetingController.cs
@@ -10,13 +10,12 @@ namespace OdeToFood.Web.Controllers
 {
     public class GreetingController : Controller
     {
-        // GET: Greeting
-        public ActionResult Index()
+        //GET: Greeting
+        public ActionResult Index(string name)
         {
-            var model = new GreetingViewModel
-            {
-                Message = ConfigurationManager.AppSettings["message"]
-            };
+            var model = new GreetingViewModel();
+            model.Name = name ?? "no name";
+            model.Message = ConfigurationManager.AppSettings["message"];
             return View(model);
         }
     }
